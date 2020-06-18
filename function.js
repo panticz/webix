e7336f66d6f22dcba2f7df18807ec46c45cc2aac
panticz <mail@konczalski.de>
2020-06-18 16:36:36 +0200

    Add function.js

---
+++ function.js
@@ -0,0 +1,23 @@
+function getUrlParameter(name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    var results = regex.exec(url);
+    if (!results)
+        return null;
+    if (!results[2])
+        return '';
+
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function printLink(url, txt) {
+    if (txt !== null) {
+        return "<a href='" + url  + txt + "'>" + txt + "</a>";
+    } else {
+        return "";
+    }
+}
